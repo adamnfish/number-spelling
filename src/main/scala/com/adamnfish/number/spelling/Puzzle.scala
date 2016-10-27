45e813025395f4b6fa0322396f67b4adc087d64b
Adam Fisher <adam.fisher@guardian.co.uk>
2016-10-27 13:30:05 +0100

    duh, `sorted`

--- src/main/scala/com/adamnfish/number/spelling/Puzzle.scala
+++ src/main/scala/com/adamnfish/number/spelling/Puzzle.scala
@@ -4,8 +4,6 @@ import zillion.cardinal
 
 object Puzzle extends App {
   println {
-    Stream.range(0, 10000000).map(cardinal(_)).find {
-      _.foldLeft("abcdefghijklmnopqrstuvwxyz|")((r, c) => if (r.contains(c)) r.dropWhile(_ != c) else "").nonEmpty
-    }
+    Stream.range(0, 10000000).map(cardinal(_)).find(n => n == n.sorted)
   }
 }
