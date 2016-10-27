9548f36b85b2611639ea183ae69a87f357c72cae
Adam Fisher <adam.fisher@guardian.co.uk>
2016-10-27 12:30:47 +0100

    Shorten everything at the expense of clarity

--- build.sbt
+++ build.sbt
@@ -6,8 +6,6 @@ scalaVersion := "2.11.8"
 
 resolvers += "bintray/non" at "http://dl.bintray.com/non/maven"
 
-// Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
-  "org.spire-math" %% "zillion" % "0.1.2",
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  "org.spire-math" %% "zillion" % "0.1.2"
 )
--- src/main/scala/com/adamnfish/number/spelling/Puzzle.scala
+++ src/main/scala/com/adamnfish/number/spelling/Puzzle.scala
@@ -2,15 +2,10 @@ package com.adamnfish.number.spelling
 
 import zillion.cardinal
 
-object Puzzle {
-  def main(args: Array[String]): Unit = println(lowestAlphabeticalNumber)
-
-  def lowestAlphabeticalNumber = {
-    Stream.range(0, 10000000).map(cardinal(_)).find { name =>
-      name.foldLeft("abcdefghijklmnopqrstuvwxyz" -> true) { case ((remainingLetters, valid), char) =>
-        if (remainingLetters.contains(char)) remainingLetters.dropWhile(_ != char) -> true
-        else "" -> false
-      }._2
+object Puzzle extends App {
+  println {
+    Stream.range(0, 10000000).map(cardinal(_)).find {
+      _.foldLeft("abcdefghijklmnopqrstuvwxyz|")((r, c) => if (r.contains(c)) r.dropWhile(_ != c) else "").nonEmpty
     }
   }
 }
